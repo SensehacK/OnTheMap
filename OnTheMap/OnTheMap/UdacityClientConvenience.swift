109162ab58368ceea2ba9c57778862b58acca858
SensehacK <kautilyasave@gmail.com>
2016-11-19 15:59:01 +0530

    1st Issue Resolved. Sub data was not used.
    Another Issue Encountered.
    Fellow Slack member helped me mitigate the issue.

--- OnTheMap/OnTheMap/UdacityClientConvenience.swift
+++ OnTheMap/OnTheMap/UdacityClientConvenience.swift
@@ -79,6 +79,8 @@ class UdacityClientConvenience {
                 return
             }
             
+            let userData = data.subdata(in : Range(5...Int(data.count)))
+            
             // Parsing data
             let parsedData : AnyObject
            
@@ -87,7 +89,7 @@ class UdacityClientConvenience {
             
             
             do {
-                parsedData = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as! NSDictionary
+                parsedData = try JSONSerialization.jsonObject(with: userData, options: .allowFragments) as! NSDictionary
                 
                 // Console Debug Prints
                 print("In func getUserSessionKey PArsing Data with JSONSerialization  task network request ")
@@ -147,15 +149,16 @@ class UdacityClientConvenience {
             
             let range = Range(uncheckedBounds: (5, data.count - 5))
             let newData = data.subdata(in: range) /* subset response data! */
-            print(NSString(data: newData, encoding: String.Encoding.utf8.rawValue)!)
+            //print(NSString(data: newData, encoding: String.Encoding.utf8.rawValue)!)
 
-            
+            let userData2 = data.subdata(in : Range(5...Int(data.count)))
+
             // Parse Data & store it in User Info.swift
             
             let parsedData : AnyObject
 
             do {
-                parsedData = try JSONSerialization.jsonObject(with: newData, options: .allowFragments) as! NSDictionary
+                parsedData = try JSONSerialization.jsonObject(with: userData2, options: .allowFragments) as! NSDictionary
             } catch {
                 print("Error Catched in Parsing Data")
                 completionHandlerforIdentifyUserWithSessionKey(false, "Error Parsing Data")
