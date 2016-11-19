eb4090db845cfce890f1276125f2ef30aef2d3bf
SensehacK <kautilyasave@gmail.com>
2016-11-19 11:41:18 +0530

    Completed Map View Controller

--- OnTheMap/OnTheMap/MapViewController.swift
+++ OnTheMap/OnTheMap/MapViewController.swift
@@ -14,6 +14,7 @@ class MapViewController : UIViewController , MKMapViewDelegate {
     @IBOutlet weak var mapViewController: MKMapView!
     
     
+    // let parsedClient = ParsingClient.sharedInstance() 
     
     
     
@@ -21,9 +22,12 @@ class MapViewController : UIViewController , MKMapViewDelegate {
         <#code#>
     }
     
+    // MARK: View Will appear
     override func viewWillAppear(_ animated: Bool) {
         
         super.viewWillAppear(animated)
+        
+        ParsingClient.sharedInstance().getUserLocation(userIDUniqueKey: UserInfo.userKey) { (success, error) in
         let tempLatitude = UserInfo.MapLatitude
         let tempLongitude = UserInfo.MapLongitude
         
@@ -37,8 +41,102 @@ class MapViewController : UIViewController , MKMapViewDelegate {
                 self.mapViewController.setRegion(coordinateRegion, animated: true)
                 
             }
+            
+        } else {
+            self.displayAlertHelper(message: error!)
         }
     }
     
+        
+        // Get Multiple Student Locations 
+        
+        
+        ParsingClient.sharedInstance().getStudentsLocation() { ( success , error) in
+            
+            if success! {
+                // Remove old pins & Reinitialize the map with refreshed Pins
+                self.mapViewController.removeAnnotations(self.mapViewController.annotations)
+                self.reinitializedPopulateMap()
+            } else {
+                self.displayAlertHelper(message: error!)
+            }
+        
+        }  // getStudentsLocation End declaration
+        
     
-}
+    }  // View will appear End declaration 
+    
+    
+    // IBACtions 
+    
+    // MARK: Logout Button IBAction
+    
+    @IBAction func logoutButtonPressed(_ sender: AnyObject) {
+        UdacityClientConvenience.sharedInstance().deleteUserSession() { ( success  , error ) in
+            
+            if success {
+                performUIUpdatesOnMain {
+                    self.dismiss(animated: true, completion: nil)
+                }
+            } else {
+                self.displayAlertHelper(message: error!)
+            }
+            
+        }//  Delete USer Session End Declaration
+        
+    } //  Logout End Declaration
+    
+    
+    // MARK: Refresh Button IBAction
+    @IBAction func refreshButtonPressed(_ sender: AnyObject) {
+        
+        ParsingClient.sharedInstance().getStudentsLocation() { ( success ,error) in
+            
+            if success! {
+                performUIUpdatesOnMain {
+                   self.mapViewController.removeAnnotations(self.mapViewController.annotations)
+                    self.reinitializedPopulateMap()
+                }
+            } else {
+                self.displayAlertHelper(message: error!)
+            }
+            
+        } // get Student Location End Declaration
+
+    } // Refresh button End Declaration
+
+    
+    // MARK: Function to ReInitialised  map with students Locations Markers
+    
+    func reinitializedPopulateMap(){
+        
+        // Store map annotations
+        var annotations = [MKPointAnnotation]()
+        
+        for ss in StudentStructDict.sharedInstance.studentsDict {
+            
+            // Create coordinate from latitude and longitude
+            let sslat = CLLocationDegrees(ss.latitude)
+            let sslon = CLLocationDegrees(ss.longitude)
+            let coordinate = CLLocationCoordinate2D(latitude: sslat, longitude: sslon)
+            
+            // Create map annotation with coordinate, name and media URL
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = coordinate
+            annotation.title = "\(ss.firstName) \(ss.lastName)"
+            annotation.subtitle = ss.mediaURL
+            
+            // Add annotation to array
+            annotations.append(annotation)
+        }
+        
+        // Add annotations to map
+        mapViewController.addAnnotations(annotations)
+        
+    } // func reinitializedPopulateMap() end
+
+    
+    
+    
+
+} // Class End declaration
